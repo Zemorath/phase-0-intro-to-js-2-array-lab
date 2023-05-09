c3490c17f483f58606a1e1b2aae50860d3abf62e
Zemorath <trentward10@gmail.com>
2023-05-08 19:05:51 -0500

    Array Lab Complete

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat() {
+    cats.push('Ralph');
+}
+
+function destructivelyPrependCat() {
+    cats.unshift('Bob');
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop(cats.length-1);
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift(0);
+}
+
+function appendCat() {
+    let newCats =  [...cats, "Broom"];
+    return newCats;
+}
+
+function prependCat() {
+    let newCats = ['Arnold', ...cats];
+    return newCats;
+}
+
+function removeLastCat() {
+    let newCats = cats.slice(0, cats.length-1);
+    return newCats;
+}
+
+function removeFirstCat() {
+    let newCats = cats.slice(1, cats.length);
+    return newCats;
+}
